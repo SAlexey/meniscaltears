145629e716870e94d23e2a39ede585d52b309fa3
Alexey Shestakov <shestakov@zib.de>
2021-07-15 11:24:09 +0200

    trilinear interpolation

--- data/transforms.py
+++ data/transforms.py
@@ -92,7 +92,7 @@ def resize_volume(img, size, tgt=None):
 
     zoom = (1, sx / ox, sy / oy, sz / oz)
 
-    img = F.interpolate(img.unsqueeze(0), size).squeeze(0)
+    img = F.interpolate(img.unsqueeze(0), size, mode="trilinear").squeeze(0)
 
     if tgt is not None:
         _assert_tgt(tgt)
