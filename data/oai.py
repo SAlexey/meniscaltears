319723e25b4e39c0cb7eeed8becc32c22d0ee5ba
SAlexey <shestakov.alexy@gmail.com>
2021-08-10 22:37:10 +0200

    import fix

--- data/oai.py
+++ data/oai.py
@@ -14,6 +14,7 @@ import math
 from util.box_ops import box_cxcywh_to_xyxy, denormalize_boxes
 from torch.utils.data import DataLoader
 from .transforms import *
+from util.misc import _is_sequence
 
 
 class CropDataset(Dataset):
